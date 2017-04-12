61e37323f8cd531995ec28ee474d69ea8b79b566
yk10850 <yk10850@ly.com>
2017-04-12 15:34:02 +0800

    MVCControl

---
+++ MyControls/MyControls/MyControlOne.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyControls.MyControls
+{
+    public static class MyControlOne
+    {
+        /// <summary>
+        /// RadioButton
+        /// </summary>
+        /// <param name="nametarget">name属性</param>
+        /// <param name="idtarget">id属性</param>
+        /// <returns></returns>
+        public static HtmlString RadioButton(this HtmlHelper helper,string nametarget, string idtarget)
+        {
+            string str = String.Format("<input type='radio' name='{0}' id='{1}' />", nametarget, idtarget);
+            return new HtmlString(str);
+        }
+    }
+}
